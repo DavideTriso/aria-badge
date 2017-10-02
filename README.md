942f8b2f85d53ba0fee1c7c3d2dca011fff10b94
DavideTriso <davidetrisolinI@yahoo.it>
2017-10-02 22:57:08 +0200

    started

---
+++ README.md
@@ -0,0 +1,91 @@
+# BADGE
+
+## About
+
+simple jQuery plugin for badges. [Go to demo page](https://davidetriso.github.io/badge/).
+
+* User-friendly and accessible
+* Less than 2KB (minified).
+* Compatible with amd and require.js
+
+## Dependencies
+
+**jQuery**
+
+Developed and tested with jQuery 3.2.1
+
+
+## Settings / Options
+
+Name | Default | Type | Description
+-----|---------|------|-------------
+updatedClass | badge_updated | string | The class added to the badge when a value update occurs.
+updatedClassRemoveAfter | 3000 | int > 0 | Remove the updatedClass after the time passed.
+hiddenClass | badge_hidden | string | The class added to the badge when `hideWhenZero` is set to true and the badge's value is 0, null, undefined or false.
+aLabel | You have {X} notifications | string | The accessible label for the badge (`{X}` will be replaced with the value passed with `initialValue` or to the method `update`).
+useTitle | true | bool | Use the accessible label also as title attribute.
+initialValue | 0 | string or int | Initial value of the badge.
+hideWhenZero | false | bool | When true the badge will be hidden by adding the `hiddenClass` and setting `aria-hidden="true"` when the badge's value is 0, null, undefined or false.
+
+## Usage
+
+1. Include the JS script **badge.js** - or the minified production script **badge.min.js**-  in the head or the body of your HTML file.
+2. Include the CSS file  **badge.css** in the head of your HTML file or include the SCSS files in your project.
+3. Initialise the widget within an inline script tag, or in an external JS file.
+
+### HTML
+
+Use following HTML markup to implement a badge:
+
+```html
+<span class="badge"></span>
+```
+
+**IMPORTANT**:  If a region of the page is responsible for the update of the badge, it is necessary to expose the relationship between the badge and the page region with the `aria-controls` attribute for accessibility. 
+
+### JS: Initialise
+
+Initialise the plugin as follows:
+
+```javascript
+$('.badge').badge({
+  option1: value1,
+  option2: value2
+});
+```
+
+## Methods
+
+The plugin supports following methods: update.
+
+### Update:
+
+To update a badge call `badge` and pass **'update'** as first parameter and the new value as second parameter:
+
+```javascript
+$('#my-badge').badge('update', 10);
+```
+
+## Custom events
+
+This plugin triggers following events:
+
+* **badge.initialised** after the badge is initialised.
+* **badge.updated** when the badge is updated.
+* 
+
+The custom events are triggered on window and return the badge data object as argument.
+
+```javascript
+//add event listener  
+$(window).on('badge.initialised', function(event, badge){
+  //perform an action
+  badge.element.addClass('my-custom-class');
+});
+```
+
+## LICENSE
+
+This project is licensed under the terms of the **MIT license**.
+
+See [LICENSE.md](LICENSE.md) for detailed informations.
---
+++ src/js/badge.js
@@ -0,0 +1,198 @@
+/*
+ * # MIT License
+
+Copyright (c) 2017 Davide Trisolini
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+ */
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    }
+}(function ($, window) {
+    'use strict';
+    var pluginName = 'badge', // the name of the plugin
+      a = {
+          aLi: 'aria-live',
+          aAt: 'aria-atomic',
+          aRe: 'aria-relevant',
+          aLab: 'aria-label',
+          tit: 'title',
+          aHi: 'aria-hidden',
+          t: 'true',
+          f: 'false'
+      },
+      win = $(window);
+
+
+    //-----------------------------------------
+    // The actual plugin constructor
+    function Badge(element, userSettings) {
+        var self = this;
+
+        self.settings = $.extend({}, $.fn[pluginName].defaultSettings, userSettings);
+        self.element = $(element);
+        self.elementStatus = false; //true when visible and value is positive, false when value is 0, null, undefined or false
+
+        //init form
+        self.init();
+        self.update(self.settings.initialValue);
+    };
+
+
+    // Avoid Plugin.prototype conflicts
+    $.extend(Badge.prototype, {
+        init: function () {
+            //add attributes
+            this.element
+                .attr('role', 'status')
+                .attr(a.aLi, 'polite')
+                .attr(a.aAt, a.t);
+
+
+            //trigger custom event on window for authors to listen for
+            win.trigger(pluginName + '.initialised', [self]);
+        },
+        setValue: function (value) {
+            var self = this,
+                settings = self.settings,
+                element = self.element,
+                label = settings.aLabel.replace('{X}', value); //replace {X} with real value to generate accessible label.
+
+            //update value
+            element
+                .text(value)
+                .attr(a.aLab, label);
+
+            //update title attribute if useTitle option is enabled
+            if (settings.useTitle) {
+                element.attr(a.tit, label);
+            }
+
+            //add updated class and remove after time passed from author
+            element.addClass(settings.updatedClass);
+
+            setTimeout(function () {
+                element.removeClass(settings.updatedClass);
+            }, settings.updatedClassRemoveAfter);
+        },
+        show: function () {
+            var self = this;
+
+            self.element
+                .removeClass(self.settings.hiddenClass)
+                .attr(a.aHi, a.f);
+        },
+        hide: function () {
+            var self = this;
+
+            self.element
+                .addClass(self.settings.hiddenClass)
+                .attr(a.aHi, a.t);
+        },
+        update: function (value) {
+            var self = this;
+
+            if (value !== null && value !== false && value !== undefined && value != 0) {
+                if (!self.elementStatus) {
+
+                    self.elementStatus = true;
+
+                    if (self.settings.hideWhenZero) {
+                        self.show();
+                    }
+                }
+
+                self.setValue(value);
+            } else {
+
+
+                if (self.elementStatus) {
+                    self.elementStatus = false;
+                }
+
+                if (self.settings.hideWhenZero) {
+                    self.hide();
+                }
+
+                self.setValue(value);
+            }
+
+            //trigger custom event on window for authors to listen for
+            win.trigger(pluginName + '.updated', [self]);
+        },
+        //-------------------------------------------------------------
+        //Method caller
+        //-------------------------------------------------------------
+        methodCaller: function (methodName, methodArg) {
+            var self = this;
+
+            switch (methodName) {
+                case 'update':
+                    self.update(methodArg);
+                    break;
+            }
+        }
+    });
+
+
+
+
+    // A really lightweight plugin wrapper around the constructor,
+    // preventing against multiple instantiations
+    $.fn[pluginName] = function (userSettings, methodArg) {
+        return this.each(function () {
+            var self = this;
+            /*
+             * If following conditions matches, then the plugin must be initialsied:
+             * Check if the plugin is instantiated for the first time
+             * Check if the argument passed is an object or undefined (no arguments)
+             */
+            if (!$.data(self, 'plugin_' + pluginName) && (typeof userSettings === 'object' || typeof userSettings === 'undefined')) {
+                $.data(self, 'plugin_' + pluginName, new Badge(self, userSettings));
+            } else if (typeof userSettings === 'string') {
+                $.data(self, 'plugin_' + pluginName).methodCaller(userSettings, methodArg);
+            }
+        });
+    };
+
+
+    $.fn[pluginName].defaultSettings = {
+        updatedClass: 'badge_updated',
+        updatedClassRemoveAfter: 3000, //
+        hiddenClass: 'badge_hidden',
+        aLabel: 'You have {X} notifications',
+        useTitle: true,
+        initialValue: 0,
+        hideWhenZero: false
+    };
+}(jQuery, window)));
+
+$(window).ready(function () {
+    'use strict';
+
+    //init badge
+    $('form').badge({});
+
+    //update badge
+    $('form').badge('update', 10);
+
+});
